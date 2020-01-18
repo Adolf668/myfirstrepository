a485c3779f8e6ef6e386c500abf5eebeaf7d5be6
MR MENAANU <MR MENAANU@DESKTOP-I0G7FJ0>
2020-01-17 16:09:39 -0800

    a trial

---
+++ src/org/amalitech/adolfself_17122019/AdolfSelf_17122019.java
@@ -0,0 +1,59 @@
+package org.amalitech.adolfself_17122019;
+
+public class AdolfSelf_17122019 {
+	public static void main(String [] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("Hello Adolf");
+		System.out.print("Hello World");
+		System.out.println("Adolf");
+		
+
+		
+		int answer;
+		answer = 2+2;
+		System.out.println("2 plus 2 is "  +answer);
+		
+		//First self-test exercises on page 8 of chapter1
+		
+		//Qstn (1) If the following statement were used in a Java program, it would cause something
+		//to be written to the screen. What would it cause to be written to the screen?
+				System.out.println("Java is not a drink.");
+				// Answer: Java is not a drink.
+				
+		//Qstn (2) Give a statement or statements that can be used in a Java program to write the following
+		//to the screen:
+		//(i)I like Java.
+		//(ii)You like tea.
+				
+		// Answer
+				System.out.println("I like Java");
+				System.out.println("You like tea");
+		
+		//Qstn (3) Write a complete Java program that uses System.out.println to output the following
+		//to the screen when run:
+		// Hello World!
+		
+		// Answer
+		
+		// public static void main(String [] args) {
+				
+	
+		System.out.println("Hello World");
+	    //}
+	
+				
+		char a, b;
+		a = 'b';
+		System.out.println(a);
+		b = 'c';
+		System.out.println(b);
+		a = b;
+		System.out.println(a);
+				
+	}
+	}
+	
+
+ 
+ 
